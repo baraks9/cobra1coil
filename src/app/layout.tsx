12918308d62030a619655f5377f2e6b9ff6aba09
barak <polish.dr@gmail.com>
2026-01-30 15:57:54 +0200

    feat: add Google Tag Manager to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Heebo, Assistant } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -14,6 +15,8 @@ const assistant = Assistant({
   variable: "--font-assistant",
   subsets: ["hebrew", "latin"],
 });
+
+const GTM_ID = "GTM-MD894JV";
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'https://cobra1.co.il'),
@@ -31,9 +34,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="he" dir="rtl">
+      <head>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body
         className={`${heebo.variable} ${assistant.variable} antialiased font-sans flex flex-col min-h-screen`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         <div className="flex-grow">
           {children}
